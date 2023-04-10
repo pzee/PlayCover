dc26e217d3bcea62716deb95e0f0beb985b37162
Hao Guan <10684225+hguandl@users.noreply.github.com>
2023-04-11 01:09:04 +1000

    Use slowlane to publish

--- slowlane.sh
+++ slowlane.sh
@@ -3,6 +3,8 @@ set -eu -o pipefail
 
 rm -rf build
 
+### Build PlayCover
+
 xcodebuild clean archive -scheme PlayCover -configuration Release -archivePath build/PlayCover.xcarchive
 
 APP_BUNDLE="build/PlayCover.xcarchive/Products/Applications/PlayCover.app"
@@ -11,12 +13,35 @@ codesign -s "Developer ID Application: Hao Guan (29V29Y67P2)" -f -o runtime --de
 codesign -s "Developer ID Application: Hao Guan (29V29Y67P2)" -f -o runtime --deep --timestamp $APP_BUNDLE/Contents/Frameworks/Sparkle.framework
 codesign -s "Developer ID Application: Hao Guan (29V29Y67P2)" -f -o runtime --deep --timestamp $APP_BUNDLE
 
+### Package PlayCover
+
 mkdir -p build/PlayCover
 ln -sfh /Applications build/PlayCover/Applications
 mv $APP_BUNDLE build/PlayCover
 hdiutil create -srcfolder build/PlayCover -format UDBZ build/PlayCover.dmg
 
+### Notarize PlayCover
+
 xcrun notarytool submit build/PlayCover.dmg --keychain-profile "HG_NOTARY_PWD" --wait
 xcrun stapler staple build/PlayCover.dmg
-mv build/PlayCover.dmg build/PlayCover-$(date -u +"%Y%m%dT%H%M%SZ").dmg
-open build
+
+### Upload PlayCover
+
+rm -rf update/updates
+mkdir -p update/updates
+
+TAG_NAME=$(git describe --tags --abbrev=0)
+mv build/PlayCover.dmg update/updates/PlayCover-$TAG_NAME.dmg
+gh release create $TAG_NAME update/updates/PlayCover-$TAG_NAME.dmg --title "$TAG_NAME" --generate-notes
+
+### Publish PlayCover
+
+cd update
+./sparkle/bin/generate_appcast \
+    --download-url-prefix 'https://github.com/PlayCover/PlayCover/releases/download/$TAG_NAME/' \
+    --link 'https://github.com/PlayCover/PlayCover/releases/tag/$TAG_NAME' \
+    --full-release-notes-url 'https://github.com/PlayCover/PlayCover/releases/tag/$TAG_NAME' \
+    -o appcast.xml ./updates
+git add appcast.xml
+git commit -S -m "Update appcast.xml"
+git push origin update
